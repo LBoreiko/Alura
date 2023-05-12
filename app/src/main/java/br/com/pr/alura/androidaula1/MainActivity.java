f276a277e8845d1f48830f3c608bb342b6d94b81
LBoreiko <123508853+LBoreiko@users.noreply.github.com>
2023-05-12 17:20:51 -0300

    Commit ListView

--- app/src/main/java/br/com/pr/alura/androidaula1/MainActivity.java
+++ app/src/main/java/br/com/pr/alura/androidaula1/MainActivity.java
@@ -1,19 +1,24 @@
 package br.com.pr.alura.androidaula1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Leonardo Boreiko");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Leonardo Boreiko",
+                "Fernanda Marques", "Gabriela Marques Boreiko", "Max", "Estrela"));
+        ListView listaAlunos = findViewById(R.id.activity_main_lista_alunos);
+        listaAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
     }
 }
