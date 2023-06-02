45555650b87b0a354b779fe4db7500b3cf6b6b78
LBoreiko <123508853+LBoreiko@users.noreply.github.com>
2023-06-02 18:29:29 -0300

    Commit Utilizar o inspetor de código do Android Studio;

--- app/src/main/java/br/com/pr/alura/listadealunos/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/pr/alura/listadealunos/ui/activity/FormularioAlunoActivity.java
@@ -6,14 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.Serializable;
 
 import br.com.pr.alura.androidaula1.R;
 import br.com.pr.alura.listadealunos.dao.AlunoDao;
